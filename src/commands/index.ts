4bc57fc13151255036f90f5de3804ed75208870f
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-05-16 21:57:05 +0100

    Add build, start, and dev scripts to package.json; implement command exports in index.ts

---
+++ src/commands/index.ts
@@ -0,0 +1,9 @@
+import { startCommand } from './startCommand';
+import { latestMoviesCommand } from './latestMoviesCommand';
+import { latestSeriesCommand } from './latestSeriesCommand';
+
+export {
+  startCommand,
+  latestMoviesCommand,
+  latestSeriesCommand
+};
